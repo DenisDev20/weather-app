0140d499d521b9585cbf1b2a46ae47d456679dbf
DenisDev20 <96378887+DenisDev20@users.noreply.github.com>
2022-11-28 18:30:41 +0300

    Update unit_14.js

--- unit_14.js
+++ unit_14.js
@@ -1,76 +1,21 @@
-
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=Aktau&appid=3ad9caeccb3e145faa476cd45cfa696d')
-//     .then(function (resp) { return resp.json() })
-//     .then(function (data) {
-//         console.log(data)
-//         let city = document.querySelector('.city-name')
-//         city.innerHTML = data.name
-//         let temp = document.querySelector('.degrese')
-//         temp.innerHTML = Math.floor((data.main.temp) - 273) + '&deg';
-//     })
-//     .catch(function () {
-
-//     });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     let val = document.getElementById('city_select').value;
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=3ad9caeccb3e145faa476cd45cfa696d`)
-//         .then(function (resp) { return resp.json() })
-//         .then(function (data) {
-//             console.log(data)
-//             let city = document.querySelector('.city-name')
-//             city.innerHTML = data.name
-//             let temp = document.querySelector('.degrese')
-//             temp.innerHTML = Math.floor((data.main.temp) - 273) + '&deg';
-//             let clouds = document.querySelector('.clouds')
-//             let getDataClouds = data.weather[0].description;
-//             let string = String(getDataClouds)
-
-//             clouds.textContent = ucFirst(string);
-
-//             getIcon(data);
-
-
-//         })
-//         .catch(function () {
-
-//         });
-// });
-
-// document.getElementById('city_select').onchange = (e) => {
-//     let city = e.target.value;
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ad9caeccb3e145faa476cd45cfa696d`)
-//         .then(function (resp) { return resp.json() })
-//         .then(function (data) {
-//             let city = document.querySelector('.city-name')
-//             city.innerHTML = data.name
-//             let temp = document.querySelector('.degrese')
-//             temp.innerHTML = Math.floor((data.main.temp) - 273) + '&deg';
-//             let clouds = document.querySelector('.clouds')
-//             clouds.textContent = data.weather.o.description;
-//         })
-//         .catch(function () {
-
-//         });
-// }
-
+// Функция вставки иконки с погодой
 function setIcon(data) {
     let icon = document.querySelector('.icon-out')
     let getIcon = data.weather[0].icon;
     icon.innerHTML = ` <img src="http://openweathermap.org/img/wn/${getIcon}@2x.png" alt="" class="icon-weather">`
 }
-
+//Функция перевода первой буквы в заглавную
 function ucFirst(str) {
     if (!str) return str;
     return str[0].toUpperCase() + str.slice(1);
 };
-
+//Функция получения погодных сведений по городу
 function getWeather(cityName) {
     return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3ad9caeccb3e145faa476cd45cfa696d`)
         .then(function (resp) { return resp.json() })
         .then(function (data) { return data });
 }
-
+// Функция отрисовки сведений о погоде
 let showWeather = (weather) => {
     //console.log(weather)
     let city = document.querySelector('.city-name')
@@ -84,10 +29,20 @@ let showWeather = (weather) => {
 
     setIcon(weather);
 };
+//Функция для получения data атрибута при нажатии на кнопку
+let btnWeather = document.querySelectorAll('.btn-city');
+for (let i = 0; i < btnWeather.length; i++) {
+    btnWeather[i].onclick = function () {
+        let curCity = this.getAttribute('data')
+        getWeather(curCity)
+            .then(weather => showWeather(weather))
+            .catch(err => console.log(err));
+    }
+}
 
-document.getElementById('city_select').onchange = (e) => {
-    let city = e.target.value;
-    getWeather(city)
+document.addEventListener('DOMContentLoaded', () => {
+    let curCity = 'Aktau';
+    getWeather(curCity)
         .then(weather => showWeather(weather))
         .catch(err => console.log(err));
-}
+});
